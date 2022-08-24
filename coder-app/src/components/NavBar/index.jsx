0041073cf65f4bbece8b55c14d0f430c3de528d5
Gabriel Sotto <gabrielsotto90@gmail.com>
2022-08-24 03:06:24 -0300

    Navbar Menu

---
+++ coder-app/src/components/NavBar/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+
+const NavBar = () => {
+  return (
+    <nav className="NavBar">
+      <ul>
+        {/* <li><a href=""></a>Home</li>
+        <li><a href=""></a>Niños</li>
+        <li><a href=""></a>Damas</li>
+        <li><a href=""></a>Caballeros</li> */}
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
+

e3a37bfc1a438bf4aa3430a4c6964c3f5d919265
Gabriel Sotto <gabrielsotto90@gmail.com>
2022-09-03 20:28:38 -0300

    add ItemCount

--- coder-app/src/container/itemListContainer/index.jsx
+++ coder-app/src/container/itemListContainer/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import './styles.css'
+import ItemCount from '../../components/ItemCount';
+import './styles.css';
 
 const ItemListContainer = ({greeting}) => {
+
+  const agregarAlCarrito = (quantity) => {
+    console.log(`Se agregó la cantidad ${quantity} al carrito!`);
+  }
+
   return (
-    <div>
-        <h3>{greeting}</h3>
+    <div className='item-list-container'>
+        <h1>{greeting}</h1>
+        <ItemCount initial={1} stock={5} onAdd={agregarAlCarrito}/>
     </div>
   )
 }
